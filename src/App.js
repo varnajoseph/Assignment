43417a29b8f8b52414d085b4f719828eb0dd6481
varnajoseph <krishkumarkk376@gmail.com>
2023-07-04 13:24:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StudentInfo } from './StudentInfo';
+// import { DatePicker } from '@mui/lab';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>STUDENT DETAILS</h2>
+      <StudentInfo/>
+      
+    </div>
+    
+  );
+}
+
+export default App;
